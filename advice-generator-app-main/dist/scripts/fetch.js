f17ff766a59b21a37029d66ce7a876fca081483a
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-31 15:07:19 +0800

    Implemented API fetching and displaying of response

---
+++ advice-generator-app-main/dist/scripts/fetch.js
@@ -0,0 +1,21 @@
+const cardBtn = document.querySelector(".card__btn");
+
+const fetchAdvice = async () => {
+    const cardHeader = document.querySelector(".card__header");
+    const cardContent = document.querySelector(".card__advice");
+    const url = "https://api.adviceslip.com/advice";
+
+    cardBtn.classList.add("card__btn--disabled");
+    cardBtn.disabled = true;
+    await fetch(url)
+        .then(res => res.json())
+        .then(data=> {
+        cardHeader.textContent = `Advice #${data.slip.id}`;
+        cardContent.textContent = data.slip.advice;
+        })
+        .catch(error => console.log(error));
+    cardBtn.classList.remove("card__btn--disabled");
+    cardBtn.disabled = false;
+}
+
+cardBtn.addEventListener("click", fetchAdvice);
